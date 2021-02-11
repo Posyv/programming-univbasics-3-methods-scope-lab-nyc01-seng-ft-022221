224fd52ee4e5ce02a1f9a58d36edf7f58985cc0f
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-11 22:11:57 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,24 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
+mario
 
-def toadstool
-  puts status
+  def toadstool
+    status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+    puts status
+  end
+toadstool
+
+def link
+  saying = "It's Dangerous To Go Alone! Take This."
+  puts saying
 end
+link
+
+def all_phrases
+  puts phrase
+  puts status
+  puts saying
+end
+all_phrases
